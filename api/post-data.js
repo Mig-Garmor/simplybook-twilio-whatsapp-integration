92178ea3ace5cbbd76f1c8e5bd7577b8c37997d5
Mig-Garmor <thecodermiguel@gmail.com>
2024-10-18 16:38:54 +0200

    Updated structure

--- api/post-data.js
+++ api/post-data.js
@@ -1,5 +1,14 @@
 module.exports = (req, res) => {
-  const data = req.body;
-  console.log("Received data:", data);
-  res.status(200).send({ message: "Data received", receivedData: data });
+  if (req.method === "POST") {
+    const bookingData = req.body;
+
+    // Log the received booking data to Vercel's logs
+    console.log("Booking Data Received:", bookingData);
+
+    // Send a response back to Simplybook.me
+    res.status(200).json({ message: "Booking received successfully" });
+  } else {
+    // If it's not a POST request, return a 405 (Method Not Allowed)
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
 };
